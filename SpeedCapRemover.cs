64e60f3abe8081d692f916b541a353904b408e5f
bluedevGH <100230983@student.burnley.ac.uk>
2026-01-26 14:18:10 +0000

    should be fixed

--- SpeedCapRemover.cs
+++ SpeedCapRemover.cs
@@ -1,19 +1,39 @@
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
+using BepInEx.Logging;
 
 namespace UncappedSpeedMod
 {
     [BepInPlugin("com.renshei.uncapped", "Uncapped Speed", "1.1.3")]
     public class UncappedSpeedPlugin : BaseUnityPlugin
     {
+        // make the logger a field so other methods (like Update) can use it
+        private ManualLogSource speedLogs;
+
         public void Awake()
         {
-            var speedLogs = new ManualLogSource("speedLogs");
+            speedLogs = new ManualLogSource("speedLogs");
             BepInEx.Logging.Logger.Sources.Add(speedLogs);
             Harmony.CreateAndPatchAll(typeof(SpeedCapPatch));
             Logger.LogInfo("speed cap successfully removed from ultrakill");
             Logger.LogInfo("visit BlueDevGH on GitHub for the source code!");
+        }
+
+        // unity Update runs every frame to keep input handling here
+        public void Update()
+        {
+            try
+            {
+                if (Input.GetKeyDown(KeyCode.U))
+                {
+                    speedLogs.LogInfo("U key pressed");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                speedLogs.LogError($"err with {ex}");
+            }
         }
     }
 
@@ -21,34 +41,17 @@ namespace UncappedSpeedMod
     public class SpeedCapPatch
     {
         // patch upd method to constantly ensure cap for speed is increased
-        // ensures broken cap upon level change or reload/restart/respawn/honkydoodle
         [HarmonyPatch("Update")]
         [HarmonyPrefix]
         public static void RaiseSpeedLimit(NewMovement __instance)
         {
-            // set to 5000 to "remove" speed cap (just set it really high so it is never reached)
-            
+            // set to a very high value to effectively "remove" the speed cap
             __instance.walkSpeed = 1000f;
 
             if (__instance.rb != null)
             {
                 __instance.rb.drag = 0f;
             }
-            
-        }
-
-        public void Update() { //  the update space runs every frame
-        try {
-            if (Input.GetKeyDown(KeyCode.U)) {
-                speedLogs.LogInfo("U key pressed");
-            }
-        }
-        catch(System.Exception ex) {
-            speedLogs.LogError($"err with {ex}");
-        }
         }
     }
 }
-
-// this is my first real program/mod using C# so if anything breaks, please give me some time to learn!
-// using this to push an action please ignore
